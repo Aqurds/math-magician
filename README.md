c7f721658c0d8ec06c9ba67f57336e63db5931e0
aqurds <omarf1320@gmail.com>
2023-07-14 12:16:59 +0600

    Update loading, error state. Add .env for api credentials

--- README.md
+++ README.md
@@ -141,7 +141,7 @@ Clone this repository to your desired folder:
 
 Contributions, issues, and feature requests are welcome!
 
-Feel free to check the [issues page](../../issues/).
+Feel free to check the [issues page](https://github.com/Aqurds/math-magician/issues).
 
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
 
--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -4,37 +4,58 @@ import '../quote.css';
 import ApiCredentials from './ApiCredentials';
 
 function Quotes() {
-  const [quote, setQuote] = useState('Loading...');
+  const [quote, setQuote] = useState('');
+  const [error, setError] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
   const [author, setAuthor] = useState('');
   const [category, setCategory] = useState('');
   const apiFetched = useRef(false);
+
+  // the .env will be used in production
+  // const api_url = process.env.REACT_APP_API_URL;
 
   useEffect(() => {
     if (apiFetched.current) return;
     apiFetched.current = true;
     const fetchQuote = async () => {
-      const response = await fetch(ApiCredentials.URL, {
-        headers: {
-          'X-Api-Key': ApiCredentials.KEY
-        }
-      })
-      const quoteResponse = await response.json()
-      console.log(quoteResponse)
-      setQuote(quoteResponse[0].quote);
-      setCategory(quoteResponse[0].category);
-      setAuthor(quoteResponse[0].author);
+      try {
+        const response = await fetch(ApiCredentials.URL, {
+          headers: {
+            'X-Api-Key': ApiCredentials.KEY
+          }
+        })
+        const quoteResponse = await response.json()
+        // console.log(quoteResponse)
+        setQuote(quoteResponse[0].quote);
+        setCategory(quoteResponse[0].category);
+        setAuthor(quoteResponse[0].author);
+        setIsLoading(true);
+      } catch (error) {
+        setError(error.message)
+        setIsLoading(false);
+      }
     };
     fetchQuote();
 
   }, [])
 
-  
+  if (error) {
+    return (
+      <p id='quote-error'>Error: {error}</p>
+    )
+  }
 
   return (
     <div className='quote-wrapper'>
-      <p id='quote-category'>{category}</p>
-      <p id='quote-main'>{quote}</p>
-      <p id='quote-author'>{author}</p>
+      {!isLoading? (
+        <p id='quote-loading'>Quotes loading ...</p>
+      ) : (
+        <div>
+          <p id='quote-category'>{category}</p>
+          <p id='quote-main'>{quote}</p>
+          <p id='quote-author'>{author}</p>
+        </div>
+      )}
     </div>
   )
 };
