6b6c307bc502e5fea3ee077e6a7a6d4fd2ca95ae
aqurds <omarf1320@gmail.com>
2023-07-21 02:11:52 +0600

    Add Router

---
+++ src/components/Home.js
@@ -0,0 +1,48 @@
+import Navbar from './Navbar';
+import './css/home.css';
+
+function Home() {
+  return (
+    <div className='home-wrapper'>
+      <h1>
+        Welcome to the <span>Magical world</span> of<span> Math
+        </span>
+      </h1>
+      <div>
+        <p>
+          The magic of math is an awe-inspiring journey that spans the entirety of human history. From ancient civilizations to modern societies, mathematics has been a fundamental pillar of human knowledge and progress. Its roots can be traced back to prehistoric times when early humans used basic counting techniques to tally resources and track time.
+        </p>
+
+        <p>
+          As human societies evolved, so did the complexity of mathematics. Ancient civilizations like the Egyptians, Babylonians, and Greeks made significant contributions to mathematical knowledge. The Egyptians developed practical arithmetic to solve everyday problems, while the Babylonians explored advanced concepts like algebra and trigonometry. The Greeks, led by mathematicians such as Pythagoras, Euclid, and Archimedes, laid the foundations of geometry and formal logic.
+        </p>
+
+        <p>
+          During the Middle Ages, Islamic scholars preserved and expanded upon the mathematical achievements of earlier civilizations. The Islamic world made significant strides in algebra and introduced the decimal system, which ultimately revolutionized global mathematics.
+        </p>
+
+        <p>
+          The Renaissance era saw a resurgence of interest in mathematics as a powerful tool for understanding the natural world. Pioneering mathematicians like Leonardo da Vinci, Galileo Galilei, and Johannes Kepler contributed to both science and mathematics, unlocking the secrets of the universe through mathematical analysis and experimentation.
+        </p>
+
+        <p>
+          In the modern era, mathematics underwent profound transformations with the advent of calculus by Sir Isaac Newton and Gottfried Wilhelm Leibniz. This groundbreaking field revolutionized physics and engineering, enabling the study of motion, forces, and other phenomena previously thought to be beyond human comprehension.
+        </p>
+
+        <p>
+          As technology advanced, so did the application of mathematics in fields like cryptography, computer science, economics, and statistics. Mathematical concepts such as probability and optimization algorithms now underpin many aspects of modern life, from financial models to internet search engines.
+        </p>
+
+        <p>
+          The magic of math lies not only in its practical applications but also in its ability to evoke wonder and awe. It provides a universal language to express complex ideas, reveal patterns in nature, and uncover the beauty of the cosmos. Throughout history, mathematicians have marveled at the elegance of mathematical proofs and the universality of mathematical principles.
+        </p>
+
+        <p>
+          In summary, the magic of math is an extraordinary journey that has shaped human history, allowing us to explore the universe, understand our world, and build civilizations. From ancient civilizations to modern societies, mathematics continues to be an inexhaustible source of inspiration and discovery, driving innovation and progress across all domains of human endeavor.
+        </p>
+      </div>
+    </div>
+  )
+};
+
+export default Home;
